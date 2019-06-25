3279ca53c71d1c94ee690343a28ae2c6c2889fc4
_OMID_ <omid98.ir@gmail.com>
2019-06-25 12:07:21 +0430

    reales 0.00001

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function show_profile(){
+
+
+    var img=$("#user_img");
+    var b_img=$("#back_imag_id");
+
+img.addClass("img_anim");
+b_img.addClass("back_image_anim");
+
+
+
+}
+
+function un_show_profile(){
+
+
+    var img=$("#user_img");
+    var b_img=$("#back_imag_id");
+
+img.removeClass("img_anim");
+b_img.removeClass("back_image_anim");
+
+
+
+}
